f5bb7ef1eb86a99eaeb1d1bfa9d915271f79f992
ibliyhsb <catd.rojas@duocuc.cl>
2025-12-10 17:40:16 -0300

    ms-blogs-db

--- src/main/java/cl/duoc/ms_blogs_db/MsBlogsDbApplication.java
+++ src/main/java/cl/duoc/ms_blogs_db/MsBlogsDbApplication.java
@@ -6,19 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MsBlogsDbApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(MsBlogsDbApplication.class, args);
-    }
-
-}
-package cl.duoc.ms_blogs_db;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class MsBlogsDbApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MsBlogsDbApplication.class, args);
 	}
