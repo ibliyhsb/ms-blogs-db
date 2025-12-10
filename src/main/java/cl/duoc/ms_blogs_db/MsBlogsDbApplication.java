56994bdd40302971aa85bcfb77bf4207650425b4
ibliyhsb <catd.rojas@duocuc.cl>
2025-12-10 01:45:30 -0300

    proyecto base

---
+++ src/main/java/cl/duoc/ms_blogs_db/MsBlogsDbApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.ms_blogs_db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsBlogsDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsBlogsDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/duoc/ms_blogs_db/MsBlogsDbApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.ms_blogs_db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsBlogsDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
